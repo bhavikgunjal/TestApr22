8dd77a074caebe9fcc271e26640c2db419530064
bhavikgunjal <bhavikgunjal@gmail.com>
2022-07-29 08:16:29 +0530

    Change in Test1

--- src/javaProject/Tets1.java
+++ src/javaProject/Tets1.java
@@ -3,6 +3,8 @@ package javaProject;
 public class Tets1 {
 
 	public static void main(String[] args) {
+		System.out.println("Upload branch A");
+		System.out.println("Upload branch A........");
 		System.out.println(1);
 
 	}
